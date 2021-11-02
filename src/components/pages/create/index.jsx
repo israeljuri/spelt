2334fa2ccc67b2a5f0c865e5050193c6cd7634ab
Israel Juri <israeljuri0@gmail.com>
2021-11-02 13:44:47 +0100

    Update package.json file

--- src/components/pages/create/index.jsx
+++ src/components/pages/create/index.jsx
@@ -4,51 +4,48 @@ import { Link } from 'react-router-dom';
 import boy from '../../../images/boy-avatar.png';
 import girl from '../../../images/girl-avatar.png';
 
-class Create extends Component {
-  render() {
-    return (
-      <section className="create" id="create">
-        <div className="container">
-          <div className="create__content">
-            <form className="create__form  animate__animated animate__bounceInDown">
-              <header className="create__header">
-                <h3 className="create__heading">Hi, let's get your ready.</h3>
-              </header>
-              <div className="create__control">
-                <input
-                  type="text"
-                  className="create__input"
-                  placeholder="What's your nickname?"
-                />
-              </div>
-              <div className="create__gender">
-                <span className="create__atom">
-                  <input type="radio" name="gender" id="male" />
-                  <span className="avatar">
-                    <img src={boy} alt="" />
-                  </span>
+const Create = () => {
+  return (
+    <section className="create" id="create">
+      <div className="container">
+        <div className="create__content">
+          <form className="create__form  animate__animated animate__bounceInDown">
+            <header className="create__header">
+              <h3 className="create__heading">Hi, let's get your ready.</h3>
+            </header>
+            <div className="create__control">
+              <input
+                type="text"
+                className="create__input"
+                placeholder="What's your nickname?"
+              />
+            </div>
+            <div className="create__gender">
+              <span className="create__atom">
+                <input type="radio" name="gender" id="male" />
+                <span className="avatar">
+                  <img src={boy} alt="" />
                 </span>
-                <span className="create__atom">
-                  <input type="radio" name="gender" id="female" />
-                  <span className="avatar">
-                    <img src={girl} alt="" />
-                  </span>
+              </span>
+              <span className="create__atom">
+                <input type="radio" name="gender" id="female" />
+                <span className="avatar">
+                  <img src={girl} alt="" />
                 </span>
-              </div>
-              <div className="create__cta">
-                <Link to="/select-profile" className="btn" type="submit">
-                  Create Profile
-                </Link>
-                <Link to="/" className="btn btn--alt" type="submit">
-                  Go home
-                </Link>
-              </div>
-            </form>
-          </div>
+              </span>
+            </div>
+            <div className="create__cta">
+              <Link to="/select-profile" className="btn" type="submit">
+                Create Profile
+              </Link>
+              <Link to="/" className="btn btn--alt" type="submit">
+                Go home
+              </Link>
+            </div>
+          </form>
         </div>
-      </section>
-    );
-  }
-}
-
+      </div>
+    </section>
+  );
+};
 export default Create;
